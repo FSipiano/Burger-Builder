71292c8326cc8a9762f44989c466f533e00d0c8f
“faye-at-603815615736” <67593261+FSipiano@users.noreply.github.com>
2021-03-01 15:24:22 +0000

    Fixed burger!

--- burger-builder-starting-setup/src/components/Layout/Burger/BurgerIngredients/BurgerIngredients.js
+++ burger-builder-starting-setup/src/components/Layout/Burger/BurgerIngredients/BurgerIngredients.js
@@ -4,9 +4,42 @@ import style from './BurgerIngredients.css'
 
 class BurgerIngredient extends React.Component {
     render() {
-      return <div className={style.BreadBottom}></div>;
-//todo: put switch statement into render method
-    }
+
+  let ingredient = null;
+
+  switch (this.props.type) {
+        case ('bread-base'):
+            ingredient = <div className={style.BreadBottom}></ div>;
+            break;
+        case ('bread-lid'):
+            ingredient = (
+            <div className={style.BreadTop}>
+                <div className={style.Seeds1}></div>
+                <div className={style.Seeds2}></div>
+            </div>
+         );
+            break;
+        case ('meat'):
+            ingredient = <div className={style.Meat}></div>
+            break;
+        case ('cheese'):
+            ingredient = <div className={style.Cheese}></div>
+            break;
+        case ('salad'):
+            ingredient = <div className={style.Salad}></div>
+            break;
+        case ('bacon'):
+            ingredient = <div className={style.Bacon}></div>
+            break;
+        default:
+            ingredient = alert('No CSS for you!')
+    };
+return ingredient
+}
+//     {
+//       return <div className={style.BreadBottom}></div>;
+// //todo: put switch statement into render method
+//     }
 }
 
 // const burgerIngredient = (props) => {
@@ -47,6 +80,10 @@ class BurgerIngredient extends React.Component {
 // burgerIngredient.PropTypes = {
 //     type: PropTypes.string.isRequired
 // }
+
+BurgerIngredient.PropTypes = {
+  type: PropTypes.string.isRequired,
+};
 
 export default BurgerIngredient;
 
