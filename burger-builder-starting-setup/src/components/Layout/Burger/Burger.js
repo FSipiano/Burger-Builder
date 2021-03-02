16b8efea5d37dbd609eb64b9e5b1da1ead7e69fb
“faye-at-603815615736” <67593261+FSipiano@users.noreply.github.com>
2021-03-02 08:16:29 +0000

    Starting to add objects and keys

--- burger-builder-starting-setup/src/components/Layout/Burger/Burger.js
+++ burger-builder-starting-setup/src/components/Layout/Burger/Burger.js
@@ -4,11 +4,16 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredients/BurgerIngredients'
 
 const burger = (props) => {
+    const changedIngredients = Object.keys(props.Ingredients)
+        .map(ingKey => {
+            return [...Array(props.Ingredients[ingKey])].map((_, i) => {
+                return <BurgerIngredient key={ingKey + i} type={ingKey} />
+            } );
+        } );
     return (
         <div className ={classes.burger}>
             <BurgerIngredient type="bread-lid" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {changedIngredients}
             <BurgerIngredient type="bread-base" />
         </div>
     );
--- burger-builder-starting-setup/src/containers/burger builder/BurgerBuilder.js
+++ burger-builder-starting-setup/src/containers/burger builder/BurgerBuilder.js
@@ -2,11 +2,19 @@ import React from 'react';
 import Burger from '../../components/Layout/Burger/Burger';
 
 class BurgerBuilder extends React.Component {
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
+        }
+    }
     render() {
         return (
 
             <div>
-                <Burger />
+                <Burger  />
                 <div>Build Controls</div>
             </div>
         );
